3b503cb046376787f1c35870ce9f2c3640a8c6c6
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-12-16 11:39:19 +0300

    Delete files

--- generate_zip.sh
+++ generate_zip.sh
@@ -134,6 +134,7 @@ rm -rf preloader.img preloader_ck7n_h894.img preloader_emmc.img preloader_ufs.im
 # Compress recovery ROM
 echo "Compressing..."
 cd $DIR
+rm -rf .github
 file_name="CK7n_recovery_flashable_rom_A13"
 compress=$(zip -r $file_name *)
 if [[ $compress ]]; then
