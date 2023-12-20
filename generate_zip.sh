62f9909e61441379af944f57e316b2f697ca5415
YZBruh <131263805+YZBruh@users.noreply.github.com>
2023-12-20 12:24:04 +0300

    Smoother syntax

--- generate_zip.sh
+++ generate_zip.sh
@@ -9,61 +9,65 @@ set -e
 # Variables
 DIR=$(pwd)
 ARCH=$(uname -m)
-# Functions
+
+## Functions
+
 # Error function
 abort() {
-echo "Error!"
-rm -rf $DIR/recovery_rom
-cleanup
-exit 1
+    echo "Error!"
+    rm -rf $DIR/recovery_rom
+    cleanup
+    exit 1
 }
 
 # Cleaner function (values)
 cleanup() {
-unset DIR
-unset ARCH
-unset pass
-unset sparse_super
-unset sparse
-unset get_rom
-unset extract_rom
-unset source_dir
-unset destination_dir
-unset file_name
-unset compress
+    unset DIR
+    unset ARCH
+    unset pass
+    unset sparse_super
+    unset sparse
+    unset get_rom
+    unset extract_rom
+    unset source_dir
+    unset destination_dir
+    unset file_name
+    unset compress
 }
 
 # Super image unpacker function
 sparse_super() {
-echo "Sparsing super image..."
-cp $DIR/lpunpack/lpunpack.py $DIR/recovery_rom/stock/*
-sparse=$(python3 lpunpack.py super.img super)
-rm -rf $DIR/recovery_rom/stock/*/lpunpack.py
-rm -rf $DIR/lpunpack
-if [[ $sparse ]]; then
-   echo
-else
-   echo "Sparsing failed!"
-   abort
-fi
-rm -rf super.img super.unsparse.img
-cd super
-mv odm_dlkm_a.img $DIR/images/odm_dlkm_a.img
-mv odm_dlkm_b.img $DIR/images/odm_dlkm_b.img
-mv vendor_dlkm_a.img $DIR/images/vendor_dlkm_a.img
-mv vendor_dlkm_b.img $DIR/images/vendor_dlkm_b.img
-mv product_a.img $DIR/images/product_a.img
-mv product_b.img $DIR/images/product_b.img
-mv system_a.img $DIR/images/system_a.img
-mv system_b.img $DIR/images/system_b.img
-mv system_ext_a.img $DIR/images/system_ext_a.img
-mv system_ext_b.img $DIR/images/system_ext_b.img
-mv vendor_a.img $DIR/images/vendor_a.img
-mv vendor_b.img $DIR/images/vendor_b.img
-cd $DIR/recovery_rom/*
-rm -rf super
-echo "Succesfull!"
+    echo "Sparsing super image..."
+    cp $DIR/lpunpack/lpunpack.py $DIR/recovery_rom/stock/*
+    sparse=$(python3 lpunpack.py super.img super)
+    rm -rf $DIR/recovery_rom/stock/*/lpunpack.py
+    rm -rf $DIR/lpunpack
+    if [[ $sparse ]]; then
+       echo
+    else
+       echo "Sparsing failed!"
+       abort
+    fi
+    rm -rf super.img super.unsparse.img
+    cd super
+    mv odm_dlkm_a.img $DIR/images/odm_dlkm_a.img
+    mv odm_dlkm_b.img $DIR/images/odm_dlkm_b.img
+    mv vendor_dlkm_a.img $DIR/images/vendor_dlkm_a.img
+    mv vendor_dlkm_b.img $DIR/images/vendor_dlkm_b.img
+    mv product_a.img $DIR/images/product_a.img
+    mv product_b.img $DIR/images/product_b.img
+    mv system_a.img $DIR/images/system_a.img
+    mv system_b.img $DIR/images/system_b.img
+    mv system_ext_a.img $DIR/images/system_ext_a.img
+    mv system_ext_b.img $DIR/images/system_ext_b.img
+    mv vendor_a.img $DIR/images/vendor_a.img
+    mv vendor_b.img $DIR/images/vendor_b.img
+    cd $DIR/recovery_rom/*
+    rm -rf super
+    echo "Succesfull!"
 }
+
+##
 
 # Starting process
 mkdir recovery_rom
